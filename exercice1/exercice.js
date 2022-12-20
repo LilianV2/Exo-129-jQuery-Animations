b23cd7968e8b614092170ff38a8e223dbc1f782c
lilia <lilianom59@outlook.fr>
2022-12-20 12:50:26 +0100

    animjq

--- exercice1/exercice.js
+++ exercice1/exercice.js
@@ -1,4 +1,4 @@
 $("div").animate({
     left: "250px",
-    height: "500px"
-});
+    //height: "500px"
+}, 3000);
---
+++ exercice3/exercice.js
@@ -0,0 +1,5 @@
+$("div").animate({
+    width: "500px",
+    height: "500px",
+    opacity: "0.4",
+}, 3000);
